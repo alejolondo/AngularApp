143f8481c1e50a8de14d4f70e62734fcc46451b9
Alejandro Londoño <97139311+alejolondo@users.noreply.github.com>
2022-10-05 22:08:40 -0500

    add files

---
+++ src/app/gifs/resultados/resultados.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-resultados',
+  templateUrl: './resultados.component.html',
+  
+})
+export class ResultadosComponent  {
+
+  get resultados(){
+    return this.GifsService.resultados;
+  }
+  
+  constructor( private GifsService: GifsService){
+
+  }
+  
+
+}
---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SearchGifs, Gif } from '../interface/gifs.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+  
+  private apiKey: string = 'fANT2gxweTjSVAmJXVXSPiZJYtyHndnY'
+  private _historial: string[] = [];
+  
+  public resultados: Gif[] = [];
+
+  get historial(){
+    return [...this._historial]
+  }
+
+  constructor(private http: HttpClient){
+    
+    
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || []
+  }
+
+  buscarGifs( query: string = ''){
+    query = query.trim().toLowerCase();
+    
+    if(!this._historial.includes(query)){
+      
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0,10);
+      
+      localStorage.setItem('historial', JSON.stringify(this._historial));
+    }
+    
+     this.http.get<SearchGifs>(`https://api.giphy.com/v1/gifs/search?api_key=fANT2gxweTjSVAmJXVXSPiZJYtyHndnY&q=${query}&limit=10`)
+     .subscribe((resp) =>{
+      console.log(resp.data)
+      this.resultados = resp.data
+     })
+  }
+}
